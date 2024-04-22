100acaf4a0f63506891c65906810d26e9aa1f8ab
LordVader73 <40064299+divyans7@users.noreply.github.com>
2024-04-22 23:56:39 +0530

    Product Innovator

--- src/App.js
+++ src/App.js
@@ -8,16 +8,18 @@ function App() {
   return (
     <div className="demo-big-content">
     <Layout>
-        <Header className="header-color" title="Divyansh Chaudhary" scroll>
+        <Header className="header-color" scroll>
             <Navigation>
+                <Link to="/" style={{textDecoration: 'none', color: 'inherit'}}>Divyansh Chaudhary</Link>
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutMe">About Me</Link>
                 <Link to="/projects">Projects</Link>
                 <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
-        <Drawer title="Title">
+        <Drawer>
             <Navigation>
+                <Link to="/" style={{textDecoration: 'none', color: 'inherit'}}>Divyansh Chaudhary</Link>
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutMe">About Me</Link>
                 <Link to="/projects">Projects</Link>
--- src/components/CVdata.js
+++ src/components/CVdata.js
@@ -1,8 +1,8 @@
 export const CVData = {
     personalData: {
       name: 'Divyansh Chaudhary',
-      title: 'Product Expert',
-      image: 'https://scontent.fdel8-1.fna.fbcdn.net/v/t1.0-9/16602896_1571920622837072_4161117462931355705_n.jpg?_nc_cat=108&_nc_oc=AQlLcCiFX4vKACF8b06Bh3jGtBsmwQoJHKKKcP-EQDFy5Gs5r5QqRrfzaqGMyqyfdWo&_nc_ht=scontent.fdel8-1.fna&oh=320d2bfe9d0c44750fc3cd4fa16f2b64&oe=5E2EA832',
+      title: 'Product Innovator',
+      image: 'https://media.licdn.com/dms/image/C5603AQEGoGfkLPTDJg/profile-displayphoto-shrink_400_400/0/1641464381846?e=1719446400&v=beta&t=hC2sMqR2KItgp2jDGOtBFgOioupRnsH-EAQiq5-wSuA',
       contacts: [
         { type: 'email', value: 'divi.v@outlook.com' },
         { type: 'phone', value: '+91 (738) 297 1692' },
@@ -14,7 +14,7 @@ export const CVData = {
       {
         type: 'text',
         title: 'Career Profile',
-        content: ' Brilliant and creative IT professional with Bachelor’s Degree in Information Technology and passionate about creating customized solutions seeks the position of Front-end Developer in an exciting and dynamic company. With an overall experience of 2 years, including an year of experience and certifications in developing front end applications. With an aim to learn and develop new skills which will further serve the purpose of finding smart & scalable solutions.',
+        content: 'A passionate and insightful product manager with over 7 years of collective experience in driving cross‑functional teams to plan, build, launch, and manage result‑orientated products that solve consumer problems aligning in line with stakeholder’s vision',
         icon: 'usertie'
       },
       {
@@ -24,20 +24,20 @@ export const CVData = {
         items: [
           {
             title: 'Information & Technology (B.Tech)',
-            authority: 'C.C.S University',
+            authority: 'C.C.S University, Meerut, INDIA',
             authorityWebSite: 'http://ccsuniversity.ac.in/default.htm',
             rightSide: '2012 - 2016'
           },
           {
             title: 'AISSCE (12th) ',
-            authority: 'Meerut Public School',
+            authority: 'Meerut Public School, Meerut, INDIA',
             authorityWebSite: 'http://www.meerutpublicschool.edu.in/',
             rightSide: '2011 - 2012'
           },
 
           {
             title: 'AISSE (10th) ',
-            authority: 'Meerut Public School',
+            authority: 'Meerut Public School, Meerut, INDIA',
             authorityWebSite: 'http://www.meerutpublicschool.edu.in/',
             rightSide: '2009 - 2010'
           }
@@ -50,32 +50,57 @@ export const CVData = {
         icon: 'archive',
         items: [
           {
-            title: 'Associate - Front End',
+            title: 'Senior Business Analyst',
+            company: 'Broadway Infotech',
+            description: '\n- Conducted business requirements elicitation through interviews, document analysis, requirements workshops, site visits, and workflow analysis.\n- Managed agile sprints across multiple teams to drive timelines, initiatives, development, and release delivery.\n- Understood business priorities and requirements effectively and created the needed Epics, Features, Acceptance Criteria, Use Cases, and User Stories.\n- Served as a Subject Matter Expert (SME) working with key business partners in the definition, testing, training, implementation, and support of functionalities and requirements.\n- Led and managed a team of 6+ people, tracked and removed internal and external impediments; coordinated teamwork between onshore & offshore resources as a part of daily Hotfix Release Management.\n- Created and maintained Knowledge articles, training, and implementation manuals for the software product users and agents.\n- Led post-live project activities including evaluation of success criteria, identification of best practices, and strategies for future releases and adoption activities, including documentation and user training.\n- Used Fresh Works & Jira to manage the dashboards to showcase product life-cycle statuses and relevant insights.',
+            companyWebSite: 'https://www.broadwayinfotech.com/',
+            companyMeta: 'Noida, India',
+            datesBetween: 'Oct. 2021 - Jan. 2024',
+            descriptionTags: ['Business Analysis', 'Agile Methodology', 'Project Management', 'Team Leadership', 'Knowledge Management']
+          },
+          {
+            title: 'Assistant Product Manager',
+            company: 'PurplePatch Technologies (Venuemonk)',
+            description: '\n- Communicated with stakeholders, gathered requirements, and made sense of these requirements understanding business needs, business capabilities, and range of available options and provided a robust justification for the proposed solutions.\n- Managed project schedule and resource management, while ensuring accurate requirements management with the help of Slack, JIRA, and Asana.\n- Assisted in the product roadmap and release plans for those focus areas and managed expectations with all relevant stakeholders, including the senior leadership team.\n- Generated new product ideas, researched/ analyzed their feasibility and business impact, and then led the implementation of those ideas.\n- Conducted user research and usability studies to understand how customers are interacting with the product and developed enhancements based on the results/analysis.\n- Managed new product development and feature roadmap from planning to implementation by collaborating with cross-functional teams, including Technology, Engineering, Quality, Design, and Marketing.',
+            companyWebSite: 'https://www.venuemonk.com/',
+            companyMeta: 'Gurugram, India',
+            datesBetween: 'Aug. 2019 - Oct. 2021',
+            descriptionTags: ['Product Management', 'Stakeholder Management', 'Project Management', 'User Research', 'Cross-functional Team Leadership']
+          },
+          {
+            title: 'Associate - OPERATIONS EXECUTIVE',
             company: 'AT&T',
-            description: 'I\'ve worked as a Front End Developer for AT&T on contractual basis. Wherein the lions share of my task was to develop Front End for the in house business applications.',
+            description: '\n- Supported AT&T products and services to operational contractual commitments and service level agreements, on behalf of designated global strategic MNCs.\n- Had overall ownership of the Network Service Experience (customer satisfaction) and built relationships of trust and integrity with designated customers by demonstrating a comprehensive understanding both of the customer’s business requirements and the AT&T products and services.\n- Served as the point of client escalation, point of internal escalation and inquiry, and delivery - NI and 3rd party – with sufficient seniority to interact at senior levels within an organization.\n- Managed Salesforce and other AT&T CRMs single-handedly to deliver the service request to Post Sales Team.',
             companyWebSite: 'https://www.att.com/',
             companyMeta: 'Gurugram',
             datesBetween: 'August 2018 - August 2019',
-            descriptionTags: ['Javascript', 'React', 'Redux', 'Sass', 'BootStrap']
+            descriptionTags: ['Customer Service', 'Contract Management', 'Client Relations', 'Salesforce', 'CRM']
           },
           {
             title: 'Customer Service New Associate',
             company: 'Accenture',
-            description: 'Was responsible for analyzing and identifying specific instances of risk, broader risk trends and points of customer friction, developing scalable solutions for prevention.',
+            description: '\n- Was responsible for analyzing and identifying specific instances of risk, broader risk trends, and points of customer friction, developing scalable solutions for prevention.\n- Collaborated effectively with business and within TRMS and cross-functional teams to solve problems, create operational efficiencies, and deliver successfully against high organizational standards.\n- Bagged a role of SME in Transaction Risk Management Services. Played a commendable role as a mentor thus contributing to the success of the program.\n- Applied a breadth of tools, data sources, and analytical techniques to answer a wide range of high-impact business questions and proactively present new insights concisely and effectively.',
             companyWebSite: 'https://www.accenture.com/in-en',
             companyMeta: 'Gurugram',
-            datesBetween: 'August 2017 - July 2018'
-          },
-          {
-            title: 'Intern',
-            company: 'Iveda India',
-            description: 'Engaged as a Subject Matter Expert for Technology mapping of Functional Departments to scope & assess current technology deployed, pain points in business & technology acquisition needs.',
-            companyWebSite: 'http://someexamplecompany.com',
-            companyMeta: 'Noida',
-            datesBetween: 'July 2017 - August 2017'
+            datesBetween: 'August 2017 - July 2018',
+            descriptionTags: ['Risk Analysis', 'Collaboration', 'Problem Solving', 'Mentorship', 'Data Analysis']
           }
+          ,
+  {
+  title: 'Marketing Intern',
+  company: 'Iveda India',
+  description: '\n- Engaged as a Subject Matter Expert for Technology mapping of Functional Departments to scope & assess current technology deployed, pain points in business & technology acquisition needs.\n- Supported marketing campaign planning and execution.\n- Participated in marketing brainstorming sessions.',
+  companyWebSite: 'http://someexamplecompany.com',
+  companyMeta: 'Noida, India',
+  datesBetween: 'Dec. 2016 - Feb. 2017',
+  descriptionTags: ['Marketing', 'Technology Mapping', 'Campaign Planning']
+}
+
         ]
-      },
+      }
+      
+      
+      ,
       {
         type: 'projects-list',
         title: 'Projects',
@@ -83,17 +108,77 @@ export const CVData = {
         icon: 'tasks',
         groups: [
           {
-            sectionHeader: 'Company Name',
-            description: 'Optional',
+            sectionHeader: 'ImpexDocs',
+            description: 'Streamlines import/export documentation, optimizing international trade processes efficiently.',
             items: [
-              { title: 'Project', projectUrl: 'optional', description: 'Optional' },
-              { title: 'Project', projectUrl: 'optional', description: 'Optional' },
-              { title: 'Project', projectUrl: 'optional', description: 'Optional' }
+              {
+                title: 'ImpexDocs',
+                projectUrl: 'https://www.impexdocs.com/',
+                description: 'ImpexDocs provides a cloud-based software solution to companies integrating their varied export trade business processes. These solutions allow companies to automate and manage the complexity of their global trade, compliance, and logistics processes. ImpexDocs solutions and services can be used by companies dealing in exports and their associated service providers such as freight forwarders, bureaus, shipping lines, etc.'
+              }
+            ]
+          },
+          {
+            sectionHeader: 'Venuemonk',
+            description: 'Event venue booking, enhancing planning with a curated selection and seamless online reservations.',
+            items: [
+              {
+                title: 'Venuemonk',
+                projectUrl: 'https://www.venuemonk.com/',
+                description: 'VenueMonk.com is an online venue booking portal with the widest range of venues available in India and the UAE. A platform that connects customers with the best venues in their city based on their unique requirements so that they can get the ideal deal for their event. The mantra behind the product is that online venue booking is just as personal as offline venue booking, except much simpler and our goal is to provide a hassle-free online venue booking experience to our customers, all the way from browsing and shortlisting venues to booking the final one.'
+              }
+            ]
+          },
+          {
+            sectionHeader: 'Fibwave',
+            description: 'Utilizes Fibonacci analysis for precise market trend identification in trading.',
+            items: [
+              {
+                title: 'Fibwave',
+                projectUrl: 'https://www.fibwave.in/',
+                description: 'Fibwave is a fintech platform where a user can create a portfolio and invest in either bulk or monthly payments. The platform develops High Frequency Trading strategies into proprietary algorithms across various platforms. The Proprietary Algorithms help Traders, UHNI/HNI’s, Brokers, Institutional Traders invest in Low-Risk Diversified Sectors with a focus on identifying potential winning trades that would participate in the successive phases of market growth.'
+              }
+            ]
+          },
+          {
+            sectionHeader: 'The Yellow Chilli',
+            description: 'Presenter for DEFCON 20th: The Way to Go to Las Vegas.',
+            items: [
+              {
+                title: 'The Yellow Chilli',
+                projectUrl: 'https://www.theyellowchillime.com/',
+                description: 'TYC UAE is a platform under the hood of Masterchef Sanjeev Kapoor, based on the MVP model with v1 currently online and v2 waiting to be released. With a variety of dishes specially designed by Master Chef Sanjeev Kapoor who demands nothing less than perfection, The Yellow Chilli provides a delightful gastronomical experience. The design and user journey were developed keeping in mind the perfection and essence of the TYC experience.'
+              }
+            ]
+          },
+          {
+            sectionHeader: 'Vanity Kart',
+            description: 'Curates premium beauty products for discerning customers with exceptional service.',
+            items: [
+              {
+                title: 'Vanity Kart',
+                projectUrl: 'https://www.vanitykart.com/',
+                description: 'Vanity kart sells off-price ORIGINAL LEADING BRANDED merchandise Apparel, footwear and home fashion for the entire family. This MVP based product aims at synchronizing the offline and online experience of the customers.'
+              }
+            ]
+          },
+          {
+            sectionHeader: 'Zillskill',
+            description: 'Online courses for skill development, offering diverse subjects to enhance professional growth conveniently.',
+            items: [
+              {
+                title: 'Zillskill',
+                projectUrl: 'https://www.zillskill.com/',
+                description: 'ZILLSKILL is an Indian platform for all things extracurricular for kids. Online Classes, News, Daily Reads, Daily Challenges, Assessment Program. The product is aimed to target a substantial global audience with the current version limited to the Indian market for few initial releases.'
+              }
             ]
           }
         ]
-      },
-      {
+      }
+      ,
+
+
+     /* {
         type: 'common-list',
         title: 'Conferences & Certificates',
         description: '',
@@ -116,12 +201,16 @@ export const CVData = {
             authorityMeta: 'Speaker'
           }
         ]
-      },
+      },*/
       {
         type: 'common-list',
         title: 'Languages',
         icon: 'language',
         items: [
+          {
+            authority: 'Hindi',
+            authorityMeta: 'Professional'
+          },
           {
             authority: 'English',
             authorityMeta: 'Professional'
@@ -136,13 +225,53 @@ export const CVData = {
         type: 'tag-list',
         title: 'Skills Proficiency',
         icon: 'rocket',
-        items: ['React', 'Javascript', 'CSS', 'SQL', 'SomeTech', 'CoolTech']
+        items: ['React', 
+        'Javascript', 
+        'CSS', 
+        'SQL',
+        'Product Strategy and Vision',
+        'Strategic Planning',
+        'Roadmap Development',
+        'Market Research',
+        'Development and Management',
+        'Agile Methodologies',
+        'Requirements Gathering',
+        'Product Lifecycle Management',
+        'Cross-functional Collaboration',
+        'Stakeholder Engagement',
+        'Team Coordination',
+        'Technical Proficiency',
+        'MERN Stack (MongoDB, Express.js, React.js, Node.js)',
+        'Salesforce',
+        'Linux',
+        'Release Management',
+        'Versioning',
+        'Deployment Planning',
+        'Change Control',
+        'Product Owner Skills',
+        'User Story Creation',
+        'Backlog Prioritization',
+        'Sprint Planning',
+        'Analytical Skills',
+        'Data Analysis',
+        'Problem-Solving',
+        'Process Mapping',
+        'Requirements Management',
+        'Requirements Elicitation',
+        'Documentation (BRD, FRD)',
+        'Stakeholder Engagement',
+        'Communication',
+        'Stakeholder Management',
+        'Technical Skills',
+        'SQL (Database Queries and Reporting)',
+        'Salesforce (CRM Data Analysis)',
+        'Linux Proficiency']
       },
       {
         type: 'tag-list',
         title: 'Hobies & Interests',
         icon: 'cubes',
-        items: ['Photography', 'Poetry']
+        items: ['Photography', 'Poetry', 'Reading', 'Music']
       }
     ]
   }
